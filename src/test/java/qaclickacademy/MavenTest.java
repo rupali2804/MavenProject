bee3345dbf201ffffc7e495213a914a8066a0a53
rupali <rupali072@gmail.com>
2019-04-03 01:23:52 +0530

    I have committed

---
+++ src/test/java/qaclickacademy/MavenTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+public void BrowserAutomation()
+{
+	System.out.println("Browser Automation");
+}
+
+@Test
+public void elementui()
+{
+	System.out.println("Ui Elements");
+	System.out.println("Ui");
+}
+}
+
