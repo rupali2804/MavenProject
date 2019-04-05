e75d9b8866ebc870c91d1c4faf7ff6ae42510494
rupali <rupali072@gmail.com>
2019-04-05 19:26:17 +0530

    Hi I have added a new jenkinstest

---
+++ src/test/java/qaclickacademy/JenkinsTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void PostJenkins()
+	{
+		System.out.println("Post Jenkins");
+	}
+@Test
+public void Jobs() {
+	System.out.println("Jobs");
+}
+	
+	@Test
+	public void deployjobs() {
+	System.out.println("deployjobs");
+
+}
+}
+
